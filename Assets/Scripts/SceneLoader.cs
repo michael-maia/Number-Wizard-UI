cc5a50e57d0e862e048536cba36ce46b0ecb5cbc
Michael Maia <32021280+michael-maia@users.noreply.github.com>
2019-01-20 19:34:11 -0200

    Added SceneLoader funcion.

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+	public void LoadNextScene()
+    {
+        //Criando um index para as cenas através da classe SceneManager
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1); //Carrega a próxima cena
+    }
+
+    //Carregando a cena inicial caso o jogo esteja na última cena
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0); //Index = 0, é a nossa primeira cena
+    }
+}
