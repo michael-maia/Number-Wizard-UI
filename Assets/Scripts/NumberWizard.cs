61ef7427113479fa644eba8879a7e772b7c7d2bc
Michael Maia <32021280+michael-maia@users.noreply.github.com>
2019-01-29 20:21:33 -0200

    Added new functions to the 3 buttons on the game.

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -2,12 +2,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; //Necessário para a variável de tipo Text
 
 public class NumberWizard : MonoBehaviour 
 {
 
     [SerializeField] int max;
     [SerializeField] int min;
+    [SerializeField] Text guessText; //Variável criada para colocarmos o objeto GuessText dentro dela para ser exibida no jogo de acordo com o código abaixo
+
     int guess;  
 
     // Use this for initialization
@@ -18,24 +21,26 @@ public class NumberWizard : MonoBehaviour
 
     void StartGame()
     {
+        guess = (max + min) / 2; //Primeira sugestão de número
+        guessText.text = guess.ToString(); //Converte o resultado para o formato string pois assim no objeto GuessText
         max = max + 1;
-        guess = (max + min) / 2;
     }
 
     public void OnPressHigher()
     {
-        min = guess;
+        min = guess; //Muda o mínimo do nosso intervalo para ficar mais preciso o próximo "chute"
         NextGuess();
     }
 
     public void OnPressLower()
     {
-        max = guess;
+        max = guess; //Muda o máximo do nosso intervalo para ficar mais preciso o próximo "chute"
         NextGuess();
     }
 
     void NextGuess()
     {
         guess = (max + min) / 2;
+        guessText.text = guess.ToString(); //Atualizando o valor na tela
     }
 }
